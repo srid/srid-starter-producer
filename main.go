eb01aa2627eb641987497c1acc1b6cd40e072fbe
Sridhar Ratnakumar <srid@srid.ca>
2015-06-01 17:47:16 -0400

    publish entire logplex record

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"github.com/bmizerany/lpx"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
@@ -17,6 +19,7 @@ func main() {
 	router := gin.Default()
 	routerAuthed := router.Group("/", gin.BasicAuth(makeGinAccounts()))
 	routerAuthed.POST("/logs", logsReceived)
+	log.Printf("Running app at %v\b", config.Port)
 	router.Run(":" + config.Port)
 }
 
@@ -32,15 +35,31 @@ func logsReceived(c *gin.Context) {
 
 // 70 <174>1 2012-07-22T00:06:26+00:00 host erlang console - Hi from erlang
 func handleLogplexRequest(r *http.Request) error {
+	// TODO: handle frame-id to handle duplicates
 	var records []Record
 	lp := lpx.NewReader(bufio.NewReader(r.Body))
 
 	for lp.Next() {
 		header := lp.Header()
 		data := lp.Bytes()
+		json, err := encodeLogplexRecord(header, data)
+		if err != nil {
+			// TODO: don't abort, but report and continue with other records
+			return err
+		}
 		// Application name (header.Name) should be unique.
-		record := Record{partitionKey: string(header.Name), data: data}
+		record := Record{partitionKey: string(header.Name), data: json}
 		records = append(records, record)
 	}
 	return putRecords(records)
+}
+
+type LogplexLogLine struct {
+	Header *lpx.Header
+	Data   []byte
+}
+
+func encodeLogplexRecord(header *lpx.Header, data []byte) ([]byte, error) {
+	info := LogplexLogLine{header, data}
+	return json.Marshal(info)
 }
