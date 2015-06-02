64abde4f81f3bc79ea0218f643097760b0db52cc
Sridhar Ratnakumar <srid@srid.ca>
2015-06-01 23:15:09 -0400

    extract the common struct as re-usable library

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/bmizerany/lpx"
 	"github.com/gin-gonic/gin"
+	"github.com/srid/drain"
 	"log"
 	"net/http"
 )
@@ -27,6 +28,7 @@ func logsReceived(c *gin.Context) {
 	defer c.Request.Body.Close()
 	err := handleLogplexRequest(c.Request)
 	if err != nil {
+		log.Printf("ERROR: Handler error: %v\n", err)
 		c.JSON(500, gin.H{"error": err.Error()})
 	} else {
 		c.JSON(200, gin.H{})
@@ -40,26 +42,15 @@ func handleLogplexRequest(r *http.Request) error {
 	lp := lpx.NewReader(bufio.NewReader(r.Body))
 
 	for lp.Next() {
-		header := lp.Header()
-		data := lp.Bytes()
-		json, err := encodeLogplexRecord(header, data)
+		logRecord := drain.Record{lp.Header(), lp.Bytes()}
+		json, err := json.Marshal(logRecord)
 		if err != nil {
 			// TODO: don't abort, but report and continue with other records
 			return err
 		}
 		// Application name (header.Name) should be unique.
-		record := Record{partitionKey: string(header.Name), data: json}
-		records = append(records, record)
+		appToken := string(logRecord.Header.Name)
+		records = append(records, Record{partitionKey: appToken, data: json})
 	}
 	return putRecords(records)
-}
-
-type LogplexLogLine struct {
-	Header *lpx.Header
-	Data   []byte
-}
-
-func encodeLogplexRecord(header *lpx.Header, data []byte) ([]byte, error) {
-	info := LogplexLogLine{header, data}
-	return json.Marshal(info)
 }
